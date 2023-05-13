457ffc488b957bc4aa1d12a75887ac1bd6a15bf5
Lukas D'Hondt <lukas@xylys.eu>
2023-05-13 22:55:58 +0200

    update conda install script

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ fi
 
 
 chmod -v +x Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda
+bash Miniconda3-latest-Linux-x86_64.sh
 
 rm Miniconda3-latest-Linux-x86_64.sh
 
